1ae09a610d5b761415f5958f6e1b5236211153e8
Samuel Wilson <samWson@users.noreply.github.com>
2024-12-19 14:47:30 +1300

    Get environment variable, GET from API, show a chart

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,11 +4,16 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn get_env() -> String {
+    std::env::var(String::from("API_KEY")).unwrap_or(String::from(""))
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_env])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,29 @@ import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
+import { restClient } from '@polygon.io/client-js';
+const rest = restClient(await invoke("get_env"));
+
+import { CChart } from "@coreui/react-chartjs";
+
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
 
   async function greet() {
+    rest.stocks.aggregates("AAPL", 1, "day", "2023-01-01", "2023-04-14").then((data) => {
+      console.log(data);
+    }).catch(e => {
+      console.error('An error happened:', e);
+    });
+
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     setGreetMsg(await invoke("greet", { name }));
   }
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+      <h1>Welcome to Tauri & Stuff</h1>
 
       <div className="row">
         <a href="https://vitejs.dev" target="_blank">
@@ -44,6 +55,58 @@ function App() {
         <button type="submit">Greet</button>
       </form>
       <p>{greetMsg}</p>
+
+      <CChart
+        type="line"
+        data={{
+          labels: ["January", "February", "March", "April", "May", "June", "July"],
+          datasets: [
+            {
+              label: "My First dataset",
+              backgroundColor: "rgba(220, 220, 220, 0.2)",
+              borderColor: "rgba(220, 220, 220, 1)",
+              pointBackgroundColor: "rgba(220, 220, 220, 1)",
+              pointBorderColor: "#fff",
+              data: [40, 20, 12, 39, 10, 40, 39, 80, 40]
+            },
+            {
+              label: "My Second dataset",
+              backgroundColor: "rgba(151, 187, 205, 0.2)",
+              borderColor: "rgba(151, 187, 205, 1)",
+              pointBackgroundColor: "rgba(151, 187, 205, 1)",
+              pointBorderColor: "#fff",
+              data: [50, 12, 28, 29, 7, 25, 12, 70, 60]
+            },
+          ],
+        }}
+        options={{
+        //   plugins: {
+        //     legend: {
+        //       labels: {
+        //         color: getStyle('--cui-body-color'),
+        //       }
+        //     }
+        //   },
+        //   scales: {
+        //     x: {
+        //       grid: {
+        //         color: getStyle('--cui-border-color-translucent'),
+        //       },
+        //       ticks: {
+        //         color: getStyle('--cui-body-color'),
+        //       },
+        //     },
+        //     y: {
+        //       grid: {
+        //         color: getStyle('--cui-border-color-translucent'),
+        //       },
+        //       ticks: {
+        //       color: getStyle('--cui-body-color'),
+        //     },
+        //   },
+        // },
+        }}
+      />
     </main>
   );
 }
